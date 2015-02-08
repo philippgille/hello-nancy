47a1309aeb2ec25ddfd90c8068e7ee3c08202177
Philipp Gille <philipp.gille@gmail.com>
2015-02-08 14:07:11 +0100

    Added HelloWorld class; compiles and runs, responds correctly

---
+++ Hello Nancy/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hello_Nancy
+{
+    public class HelloWorld : Nancy.NancyModule
+    {
+        public HelloWorld()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
